47a115359c0800d18e5be27b34034e5acb4eaadd
Ricardo Metzger <r7metzger@gmail.com>
2024-11-02 00:46:31 -0500

    feat: tipos de datos - String

--- 01_basico/02.data_types.dart
+++ 01_basico/02.data_types.dart
@@ -1,19 +1,41 @@
 main() {
   /* Enteros */
   int a = 5;
-  print(a);
+  //print(a);
 
   /* Decimales */
   double b = 5.5;
-  print(b);
+  //print(b);
 
   /* Null Safety */
   int? c;
-  print(c);
+  //print(c);
 
   /* Operaciones */
   int d = 10;
   int e = 10;
   int result = d + e;
-  print(result);
+  //print(result);
+
+  /* String */
+
+  String name = 'Ricardo';
+  String lastName = "Metzger";
+  String test = 'Carmen\'s';
+  String test2 = "Carmen's";
+  String fullName = "$name $lastName";
+  String multiLine = """
+  Hello
+  How are you?
+  $fullName
+  """;
+  print(name);
+  print('----------');
+  print(lastName);
+  print('----------');
+  print(test);
+  print('----------');
+  print(test2);
+  print('----------');
+  print(multiLine);
 }
