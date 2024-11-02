8885915e8ad4ae9ee027eca4e2f21a19cc132af2
Ricardo Metzger <r7metzger@gmail.com>
2024-11-02 00:34:08 -0500

    feat: tipos de datos - numbers

---
+++ 01_basico/02.data_types.dart
@@ -0,0 +1,19 @@
+main() {
+  /* Enteros */
+  int a = 5;
+  print(a);
+
+  /* Decimales */
+  double b = 5.5;
+  print(b);
+
+  /* Null Safety */
+  int? c;
+  print(c);
+
+  /* Operaciones */
+  int d = 10;
+  int e = 10;
+  int result = d + e;
+  print(result);
+}
