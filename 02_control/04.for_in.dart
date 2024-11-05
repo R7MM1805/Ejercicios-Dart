d0227a564fbece5601cff09bef13b258cc04d425
Ricardo Metzger <r7metzger@gmail.com>
2024-11-05 16:43:00 -0500

    feat: ciclo for in

---
+++ 02_control/04.for_in.dart
@@ -0,0 +1,13 @@
+main() {
+  List<String> heroes = [
+    'Batman',
+    'Super Man',
+    'Iron Man',
+    'Capitan America',
+    'Mujer Maravilla',
+    'Black Widow'
+  ];
+  for (String hero in heroes) {
+    print(hero);
+  }
+}
