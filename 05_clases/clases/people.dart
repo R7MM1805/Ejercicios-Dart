e299752c6ae62202acc824c674388b140155e756
Ricardo Metzger <r7metzger@gmail.com>
2024-11-08 17:20:54 -0500

    feat: propiedades privadas en dart

--- 05_clases/clases/people.dart
+++ 05_clases/clases/people.dart
@@ -3,9 +3,11 @@ class People {
   String name = '';
   String lastName = '';
   int age = 0;
+  String _country = 'Peru';
   //Getter and Setters
   //Constructor
   //Methods
   @override
-  String toString() => 'Name: $name ¬ LastName: $lastName ¬ Age: $age';
+  String toString() =>
+      'Name: $name ¬ LastName: $lastName ¬ Age: $age ¬ Country: $_country';
 }
